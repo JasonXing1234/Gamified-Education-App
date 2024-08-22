40fd949bee7adb78e1196fcd22658085c6cf3470
Mia <miaseppi@gmail.com>
2024-08-22 15:00:40 -0600

    Sound button can now be toggled

--- lib/components/buttons/sound_button.dart
+++ lib/components/buttons/sound_button.dart
@@ -2,43 +2,48 @@ import 'package:flutter/material.dart';
 
 
 // How does this work?
-class SoundButtonListener extends ChangeNotifier {
-  var muted = false;
+// class SoundButtonListener extends ChangeNotifier {
+//   var muted = false;
+//
+//   void toggleSound() {
+//     muted = !muted;
+//     notifyListeners();
+//   }
+// }
 
-  void toggleSound() {
-    muted = !muted;
-    notifyListeners();
-  }
+class SoundButton extends StatefulWidget {
+  const SoundButton({super.key});
+
+  @override
+  SoundButtonState createState() => SoundButtonState();
 }
 
-class SoundButton extends StatelessWidget {
-  const SoundButton({
-    super.key,
-  });
+class SoundButtonState extends State<SoundButton> {
 
   final Color royalBlue = const Color(0xff2E83E8);
   final Color grey = const Color(0xff939393);
 
+  bool isMuted = false;
+
+  void toggleSound() {
+    setState(() {
+      isMuted = !isMuted;
+    });
+
+    // TODO: Logic to mute and un-mute sound
+  }
+
   @override
   Widget build(BuildContext context) {
-    // var soundState = context.watch<SoundButtonListener>();
-
-    IconData soundIcon = Icons.volume_up;
-    // if (soundState.muted) {
-    //   soundIcon = Icons.volume_mute;
-    // }
-    // else {
-    //   soundIcon = Icons.volume_up;
-    // }
 
     return IconButton(
       icon: Icon(
-        soundIcon,
+        isMuted ? Icons.volume_mute : Icons.volume_up,
         color: royalBlue,
         size: 40,
       ),
       onPressed: () {
-        // soundState.toggleSound();
+        toggleSound();
       },
     );
   }
