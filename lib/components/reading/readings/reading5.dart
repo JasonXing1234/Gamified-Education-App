81364b16a195d91044537bab6cafafeaeac27da8
Mia <miaseppi@gmail.com>
2024-09-10 11:55:47 -0600

    Adding in the reading pages for lesson 5

--- lib/components/reading/readings/reading5.dart
+++ lib/components/reading/readings/reading5.dart
@@ -15,16 +15,51 @@ const reading5 = [
     "GIVING INFORMATION",
     [
       "Social Tags determine what kind of personal information is appropriate to give to a person",
-      "Questions to ask before giving personal information",
+      "Next are some questions to ask before giving personal information",
     ],
     "no",
   ),
   ReadingPage(
-    "GIVING INFORMATION",
+    "BEFORE POSTING",
     [
-      "Social Tags determine what kind of personal information is appropriate to give to a person",
-      "Questions to ask before giving personal information",
+      "Why does the audience need the information?",
     ],
     "no",
   ),
+  ReadingPage(
+    "BEFORE REPLYING",
+    [
+      "Is this information appropriate and safe?",
+    ],
+    "no",
+  ),
+  ReadingPage(
+    "BEFORE PRIVATE MESSAGING",
+    [
+      "How will the audience use the information?",
+    ],
+    "no",
+  ),
+  ReadingPage(
+    "KNOW THE DIFFERENCE",
+    [
+      "Information you choose to give: You get to decide what information to give to people",
+
+      "Information you've been asked to give: This is information people want from you. "
+          "Sometimes you should NOT give the requested information. Other times it's safe.",
+    ],
+    "no",
+  ),
+  ReadingPage(
+    "KNOW THE DIFFERENCE",
+    [
+      "Information you choose to give: You get to decide what information to give to people",
+
+      "Information you've been asked to give: This is information people want from you. "
+          "Sometimes you should NOT give the requested information. Other times it's safe.",
+    ],
+    "no",
+  ),
+
+
 ];
