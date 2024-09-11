8e3f01294be9a128984c9d48cfa604bf262777fe
Mia <miaseppi@gmail.com>
2024-09-11 12:38:11 -0600

    Added more reading pages to Appropriate interactions lesson and merged dev into mia

--- lib/components/reading/readings/reading5.dart
+++ lib/components/reading/readings/reading5.dart
@@ -60,6 +60,220 @@ const reading5 = [
     ],
     "no",
   ),
+  ReadingPage(
+    "INFORMATION YOU GIVE",
+    [
+      "Here's some example information.\n\n"
+          "- First Name\n"
+          "- Gender\n"
+          "- Birthday (Without year)\n"
+          "- Age Range (example: 18-25)",
+
+      "You could give it to the following\n\n"
+          "- Family\n"
+          "- In Person Friends\n"
+          "- School Peers\n"
+          "- Personal Service Provides\n"
+          "- Work Peers\n"
+          "- Bosses and Teachers\n"
+          "- Online Friends\n"
+          "- Acquaintances\n"
+          "- Community Helpers\n"
+          "- Strangers",
+    ],
+    "no",
+  ),
+  ReadingPage(
+    "REQUESTED INFORMATION",
+    [
+      "Here's some example information.\n\n"
+          "- First Name\n"
+          "- Gender\n"
+          "- Birthday (Without year)\n"
+          "- Age Range (example: 18-25)",
+
+      "Only the following should request this information from you\n\n"
+          "- Family\n"
+          "- In Person Friends\n"
+          "- School Peers\n"
+          "- Personal Service Provides\n"
+          "- Work Peers\n"
+          "- Bosses and Teachers\n"
+          "- Online Friends",
+
+      "If these people request this information, DON'T give it out\n\n"
+          "- Acquaintances\n"
+          "- Community Helpers\n"
+          "- Strangers"
+    ],
+    "no",
+  ),
+  ReadingPage(
+    "INFORMATION YOU GIVE",
+    [
+      "Here's some example information.\n\n"
+          "- Photo of Yourself (non-explicit)\n"
+          "- Personal Email",
+
+      "You could give it to the following\n\n"
+          "- Family\n"
+          "- In Person Friends\n"
+          "- School Peers\n"
+          "- Personal Service Provides\n"
+          "- Work Peers\n"
+          "- Bosses and Teachers\n"
+          "- Online Friends",
+
+      "DON'T give it to these people\n\n"
+          "- Acquaintances\n"
+          "- Community Helpers\n"
+          "- Strangers",
+    ],
+    "no",
+  ),
+  ReadingPage(
+    "REQUESTED INFORMATION",
+    [
+      "Here's some example information.\n\n"
+          "- Photo of Yourself (non-explicit)\n"
+          "- Personal Email",
+
+      "Only the following should request this information from you\n\n"
+          "- Family\n"
+          "- In Person Friends",
+
+      "If these people request this information, DON'T give it out\n\n"
+          "- School Peers\n"
+          "- Personal Service Provides\n"
+          "- Work Peers\n"
+          "- Bosses and Teachers\n"
+          "- Online Friends\n"
+          "- Acquaintances\n"
+          "- Community Helpers\n"
+          "- Strangers",
+    ],
+    "no",
+  ),
+  ReadingPage(
+    "INFORMATION YOU GIVE",
+    [
+      "Here's some example information.\n\n"
+          "- Last Name\n"
+          "- Workplace\n"
+          "- School\n"
+          "- Phone Number",
+
+      "You could give it to the following\n\n"
+          "- Family\n"
+          "- In Person Friends\n"
+          "- School Peers\n"
+          "- Personal Service Provides\n"
+          "- Work Peers\n"
+          "- Bosses and Teachers",
+
+      "DON'T give it to these people\n\n"
+          "- Online Friends\n"
+          "- Acquaintances\n"
+          "- Community Helpers\n"
+          "- Strangers",
+    ],
+    "no",
+  ),
+  ReadingPage(
+    "REQUESTED INFORMATION",
+    [
+      "Here's some example information.\n\n"
+          "- Last Name\n"
+          "- Workplace\n"
+          "- School\n"
+          "- Phone Number",
+
+      "Only the following should request this information from you\n\n"
+          "- Family\n"
+          "- In Person Friends",
+
+      "If these people request this information, DON'T give it out\n\n"
+          "- School Peers\n"
+          "- Personal Service Provides\n"
+          "- Work Peers\n"
+          "- Bosses and Teachers\n"
+          "- Online Friends\n"
+          "- Acquaintances\n"
+          "- Community Helpers\n"
+          "- Strangers",
+    ],
+    "no",
+  ),
+  ReadingPage(
+    "INFORMATION YOU GIVE",
+    [
+      "Here's some example information.\n\n"
+          "- Medical History\n"
+          "- Home Address\n"
+          "- Daily Routine\n"
+          "- Full Name",
+
+      "You could give it to the following\n\n"
+          "- Family\n"
+          "- In Person Friends",
+
+      "DON'T give it to these people\n\n"
+          "- School Peers\n"
+          "- Personal Service Provides\n"
+          "- Work Peers\n"
+          "- Bosses and Teachers"
+          "- Online Friends\n"
+          "- Acquaintances\n"
+          "- Community Helpers\n"
+          "- Strangers",
+    ],
+    "no",
+  ),
+  ReadingPage(
+    "REQUESTED INFORMATION",
+    [
+      "Here's some example information.\n\n"
+          "- Medical History\n"
+          "- Home Address\n"
+          "- Daily Routine\n"
+          "- Full Name",
+
+      "Only the following should request this information from you\n\n"
+          "- Family\n"
+          "- In Person Friends",
+
+      "If these people request this information, DON'T give it out\n\n"
+          "- School Peers\n"
+          "- Personal Service Provides\n"
+          "- Work Peers\n"
+          "- Bosses and Teachers\n"
+          "- Online Friends\n"
+          "- Acquaintances\n"
+          "- Community Helpers\n"
+          "- Strangers",
+    ],
+    "no",
+  ),
+  ReadingPage(
+    "REMEMBER",
+    [
+      "Just because someone asks for information, doesn't mean you have to give it to them",
+    ],
+    "no",
+  ),
+  ReadingPage(
+    "INSECURE MESSAGING",
+    [
+      "Messaging through social media is not very secure. "
+          "This means that people with bad intentions may be able to access your messages.",
+
+      "So, even if you are messaging someone you are close with in-person (like close family), "
+          "you should still NEVER share certain pieces of information with them online.",
+
+    ],
+    "no",
+  ),
+
 
 
 ];
