f927fd0c26fdf9e60140c56b814788124aed7ec9
Mia <miaseppi@gmail.com>
2024-10-09 17:13:28 -0600

    Started adding the practice questions

---
+++ lib/components/practice/practice_questions/social_media_norms_practice/social_media_norms_practice_1.dart
@@ -0,0 +1,106 @@
+import 'package:quiz/components/question.dart';
+
+//TODO: Put in the correct answers
+
+const appropriateInteractionsPractice1 = [
+  SingleAnswerQuestion(
+  "no",
+  "Your friend wants to make a second personal account on Facebook to post memes on. "
+      "Is it ok for your friend to do that?",
+  "no",
+  [
+    "Yes",
+    "No",
+    "I don’t know"
+  ],
+  "No",
+  "Facebook has rules against making multiple accounts.",
+  ),
+  SingleAnswerQuestion(
+    "no",
+    "Who is able to make a social media account?",
+    "no",
+    [
+      "People who are older than 13",
+      "Trustworthy people",
+      "People that pass a background check",
+      "Anyone",
+    ],
+    "Anyone",
+    "It is easy for anyone to make a social media account.",
+  ),
+  SingleAnswerQuestion(
+    "no",
+    "Jill has an Instagram account where she posts pictures of her dog for a small "
+        "group of her friends to see. What kind of account is this?",
+    "no",
+    [
+      "Top/Interest Account",
+      "Alternate Personal Account (finista)",
+      "Page",
+      "Verified Celebrity or Influencer Account",
+    ],
+    "Alternate Personal Account (finista)",
+    "People use alternate personal accounts to post for a smaller group of friends.",
+  ),
+  SingleAnswerQuestion(
+    "no",
+    "What is a social media norm?",
+    "no",
+    [
+      "A way that people typically use social media",
+      "A popup ad",
+      "Business Account",
+      "A guy on social media whose name is Norman",
+      "A post on social media"
+    ],
+    "A way that people typically use social media",
+    "Norms are the way something is typically done.",
+  ),
+  SingleAnswerQuestion(
+    "no",
+    "Which kind of Facebook account allows strangers to discuss common interests?",
+    "no",
+    [
+      "Personal",
+      "Page",
+      "Facebook messenger",
+      "Group",
+    ],
+    "Group",
+    "Groups are designed for strangers with common interests to interact with each other.",
+  ),
+  SingleAnswerQuestion(
+    "no",
+    "You see a post from McDonalds on Instagram. What kind of account is this?",
+    "no",
+    [
+      "Business Account",
+      "Personal Account",
+      "Alternate Personal Account (finista)",
+      "Page",
+    ],
+    "Business Account",
+    "Business accounts are used to promote products, and McDonalds is a business "
+        "that promotes products.",
+  ),
+  SingleAnswerQuestion(
+    "no",
+    "Your cousin wants to make a second Instagram account to promote their music. "
+        "Is it ok to make multiple personal accounts on Instagram?",
+    "no",
+    [
+      "Yes",
+      "No",
+      "I don't know",
+    ],
+    "Yes",
+    "Instagram allows users to have multiple accounts",
+  ),
+
+  //TODO: Left off on question 8
+];
+
+
+
+
