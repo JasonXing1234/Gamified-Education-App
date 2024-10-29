b220b500feffbeaf185109751288ae8f04307e11
Mia <miaseppi@gmail.com>
2024-10-28 18:06:24 -0600

    Setting up better way to load lesson by giving lessons reading, quiz, and practice prameters

---
+++ lib/components/practice/practice_questions/tutorial_practice.dart
@@ -0,0 +1,107 @@
+import 'package:quiz/components/question.dart';
+
+//TODO: Put in the correct answers
+
+const appropriateInteractionsPractice1 = [
+  SingleAnswerQuestion(
+    "no",
+    "If you choose to, is it safe to give your personal phone number to a school peer over social media?",
+    "no",
+    [
+      "Yes",
+      "No"
+    ],
+    "Yes",
+    "explain",
+    //TODO: Add "Explain the right answer" parameter
+  ),
+  SingleAnswerQuestion(
+    "no",
+    "If a school peer asks for your personal email over social media, is it safe to give it to them?",
+    "no",
+    [
+      "Yes",
+      "No"
+    ],
+    "No",
+    "explain",
+  ),
+  SingleAnswerQuestion(
+    "no",
+    "Is it safe to give your social security number to close family over social media?",
+    "no",
+    [
+      "Yes",
+      "No"
+    ],
+    "No",
+    "explain",
+  ),
+  SingleAnswerQuestion(
+    "no",
+    "Your online friend asks for your address, is it safe to give it to them over social media?",
+    "no",
+    [
+      "Yes",
+      "No"
+    ],
+    "No",
+    "explain",
+  ),
+  SingleAnswerQuestion(
+    "no",
+    "A stranger online asked you to share your birth date and year with them, is it safe to give it to them?",
+    "no",
+    [
+      "Yes",
+      "No"
+    ],
+    "No",
+    "explain",
+  ),
+  SingleAnswerQuestion(
+    "no",
+    "An online friend asked you to share a picture of yourself at a football game with them, is it safe to send them this?",
+    "no",
+    [
+      "Yes",
+      "No"
+    ],
+    "No",
+    "explain",
+  ),
+
+  SingleAnswerQuestion(
+    "no",
+    "You choose to share a picture of yourself at a football game with an online friend, is it safe to send them this?",
+    "no",
+    [
+      "Yes",
+      "No"
+    ],
+    "Yes",
+    "explain",
+  ),
+  SingleAnswerQuestion(
+    "no",
+    "A school peer asks you to share your address with them, is it safe to send them this?",
+    "no",
+    [
+      "Yes",
+      "No"
+    ],
+    "No",
+    "explain",
+  ),
+  SingleAnswerQuestion(
+    "no",
+    "You choose to share your address with a school peer, is it safe to send them this?",
+    "no",
+    [
+      "Yes",
+      "No"
+    ],
+    "No",
+    "explain",
+  ),
+];
